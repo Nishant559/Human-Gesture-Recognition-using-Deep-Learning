243f34be89076b8fd7e2c12ed891b7e23f52186b
NISHANT KUMAR SINGH <55767210+Nishant559@users.noreply.github.com>
2020-01-18 18:06:21 +0530

    Add files via upload
    Classification of two activity,i.e Left Hand Front-up and Left Hand Side-up using Convolutional Neural Network.

---
+++ cnn.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 18 10:02:09 2020
+
+@author: Nishant
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 16:10:05 2020
+
+@author: Nishant
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+
+DATADIR = "C:/Graph Image/"
+CATEGORIES = ["frontup", "sideup"]
+
+for category in CATEGORIES:
+    path = os.path.join(DATADIR, category)
+    for img in os.listdir(path):
+        img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+        plt.imshow(img_array, cmap="gray")
+        plt.show()
+        break
+    break
+    
+
+
+print(img_array)
+print(img_array.shape) 
+IMG_SIZE = 100
+
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array, cmap='gray')
+plt.show()
+
+
+
+training_data = []
+
+def create_training_data():
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR, category)
+        class_num = CATEGORIES.index(category)
+        for img in os.listdir(path):
+            try:
+                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                training_data.append([new_array, class_num])
+            except Exception as e:
+                pass
+create_training_data()
+      
+
+
+print(len(training_data))
+
+
+
+import random
+
+random.shuffle(training_data)
+
+
+
+X=  [] #feature set
+y = [] #label set
+
+
+for features, label in training_data:
+    X.append(features)
+    y.append(label)
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE,1)
+
+
+
+import pickle
+
+pickle_out = open("X.pickle", "wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y.pickle", "wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+
+
+
+
+pickle_in = open("X.pickle", "rb")
+X = pickle.load(pickle_in)
+
+
+
+
+import tensorflow as tf
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import TensorBoard
+import pickle
+
+X = pickle.load(open("X.pickle","rb"))
+y = pickle.load(open("y.pickle","rb"))
+
+X = X/255.0
+
+model=Sequential()
+model.add(Conv2D(64, (3,3), input_shape = X.shape[1:]))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss="binary_crossentropy",
+              optimizer="adam",
+              metrics=['accuracy'])
+
+model.fit(X, y, batch_size=3, validation_split=0.2, verbose=1, epochs=5)
+
+
+
+
